9f43f3a39c211f07cfe280df721769f82d81526c
NithinLukose <nithinclukose12@gmail.com>
2024-05-28 07:42:56 +0530

    changing To PORT

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose
     console.log('Connection successful');
   });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
   console.log(`App running on http://localhost:${port}`);
 });
